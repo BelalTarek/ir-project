a48226d19c440ed700179e122c56048dee32d1d7
Belal Tarek <belaltarek96@gmail.com>
2019-12-07 15:38:14 +0200

    initial

---
+++ index.js
@@ -0,0 +1,6 @@
+const Corpus = require('./src/Corpus.js');
+const Document = require('./src/Document.js');
+const Similarity = require('./src/Similarity.js');
+const Stopwords = require('./src/Stopwords.js');
+
+module.exports = { Corpus, Document, Similarity, Stopwords };
